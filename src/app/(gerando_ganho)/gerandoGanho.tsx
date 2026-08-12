f7d21ce539831687b84bc6f3bbc8d231d84bd78f
ruanfabr <victor.cavalcante@grupofornecedora.com.br>
2026-08-12 17:23:26 -0300

    Ajusta padding e prepara estrutura de estilos da tela gerandoGanho

--- src/app/(gerando_ganho)/gerandoGanho.tsx
+++ src/app/(gerando_ganho)/gerandoGanho.tsx
@@ -1,12 +1,12 @@
 import { ScreenWrapper } from '@/components/ScreenWrapper'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 
 export default function GerandoGanho(){
 
     return(
         <ScreenWrapper>
-            <View>
+            <View style={{ flex:1, paddingInline: 13, paddingBlock: 15 }}>
                 <Text>
                 eae
                 </Text>
@@ -14,3 +14,8 @@ export default function GerandoGanho(){
         </ScreenWrapper>
     )
 }
+
+
+const styleContainer = StyleSheet.create({
+
+})
